48172a532b0cf5a42ac3927872c31ac99aa4fb0c
TrungKien <trungkien2003ntk@gmail.com>
2023-11-28 15:43:51 +0700

    Create First Model in FurnitureStore.Shared

--- FurnitureStore.Shared/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace FurnitureStore.Shared
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ FurnitureStore.Shared/Customer.cs
@@ -0,0 +1,35 @@
+﻿namespace FurnitureStore.Shared
+{
+    public class PurchaseHistory
+    {
+        [JsonProperty("orderID")]
+        public string? OrderID { get; set; }
+
+        [JsonProperty("date")]
+        public DateTime CreateAt { get; set; }
+
+        [JsonProperty("totalAmount")]
+        public int? TotalAmount { get; set; }
+    }
+
+    public class Customer
+    {
+        [JsonProperty("id")]
+        public string? Id { get; set; }
+
+        [JsonProperty("customerID")]
+        public string? CustomerID { get; set; }
+
+        [JsonProperty("name")]
+        public string? Name { get; set; }
+
+        [JsonProperty("phone")]
+        public string? Phone { get; set; }
+
+        [JsonProperty("totalPay")]
+        public int TotalPay { get; set; }
+
+        [JsonProperty("purchaseHistory")]
+        public List<PurchaseHistory>? PurchaseHistory { get; set; }
+    }
+}
