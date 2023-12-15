749175f13dd1ffca525265b571339a72d1789c03
Nhatduy13nhatduy <nhatduy13nhatduy@gmail.com>
2023-12-15 16:03:06 +0700

    Update Add/upadte Product Popup & custom Product Managemant Page

---
+++ FurnitureStore.Client/Pages/AdminPages/ProductManagement.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+
+namespace FurnitureStore.Client.Pages.AdminPages
+{
+    public partial class ProductManagement
+    {
+        private bool isHidden { get; set; } = true;
+
+        private void ShowAddUpdateProductPopup(MouseEventArgs e)
+        {
+            isHidden = false;
+        }
+
+        private void CloseAddUpdatePopup(MouseEventArgs e)
+        {
+            isHidden = true;
+        }
+        private void CancelAddUpdatePopup(MouseEventArgs e)
+        {
+            isHidden = true;
+        }
+    }
+}
