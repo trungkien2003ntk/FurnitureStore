77193d6c873907c0c33630e91337802f9acd65c4
TrungKien <trungkien2003ntk@gmail.com>
2023-12-19 18:23:25 +0700

    Add Controllers & more endpoints

---
+++ FurnitureStore.Server/Controllers/CategoriesController.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FurnitureStore.Shared;
+using FurnitureStore.Server.Repository;
+using FurnitureStore.Server.Interfaces;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FurnitureStore.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ILogger _logger;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CategoriesController(ILogger<CategoriesController> logger, ICategoryRepository categoryRepository)
+        {
+            this._logger = logger;
+            this._categoryRepository = categoryRepository;
+        }
+
+        // GET: api/<CategoriesController>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CategoryDTO>>> GetCategoriesAsync()
+        {
+            var categories = await _categoryRepository.GetCategoryDTOsAsync();
+
+            if (categories == null || !categories.Any()) 
+            {
+                return NotFound();
+            }
+
+            return Ok(categories);
+        }
+
+        [HttpGet("newId")]
+        public async Task<ActionResult<string>> GetNewCategoryIdAsync()
+        {
+            string newId = await _categoryRepository.GetNewCategoryIdAsync();
+
+            return Ok(newId);
+        }
+
+        // GET api/<CategoriesController>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<CategoryDTO>> GetCategoryDTOByIdAsync(string id)
+        {
+            var category = await _categoryRepository.GetCategoryDTOByIdAsync(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(category);
+        }
+
+        // POST api/<CategoriesController>
+        [HttpPost]
+        public async Task<ActionResult> CreateCategoryAsync([FromBody] CategoryDTO categoryDTO)
+        {
+            try
+            {
+                await _categoryRepository.AddCategoryDTOAsync(categoryDTO);
+
+                return Ok("Category created successfully.");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogInformation($"Error message: {ex.Message}");
+                return StatusCode(500, $"An error occurred while creating the category. CategoryId: {categoryDTO.CategoryId}");
+            }
+        }
+
+        // PUT api/<CategoriesController>/5
+        [HttpPut("{id}")]
+        public async Task<ActionResult> UpdateCategoryAsync(string id, [FromBody] CategoryDTO categoryDTO)
+        {
+            try
+            {
+                await _categoryRepository.UpdateCategoryAsync(categoryDTO);
+
+                return Ok("Category updated successfully.");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogInformation($"Error message: {ex.Message}");
+                return StatusCode(500, $"An error occurred while creating the category. CategoryId: {categoryDTO.CategoryId}");
+            }
+        }
+
+        // DELETE api/<CategoriesController>/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteCategoryAsync(string id)
+        {
+            try
+            {
+                await _categoryRepository.DeleteCategoryAsync(id);
+
+                return Ok("Category updated successfully.");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogInformation($"Error message: {ex.Message}");
+                return StatusCode(500, $"{ex.Message}");
+            }
+        }
+    }
+}
---
+++ FurnitureStore.Server/Controllers/OrdersController.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FurnitureStore.Shared;
+using FurnitureStore.Server.Interfaces;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FurnitureStore.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly ILogger<OrdersController> _logger;
+
+        public OrdersController(IOrderRepository orderRepository, ILogger<OrdersController> logger)
+        {
+            this._orderRepository = orderRepository;
+            this._logger = logger;
+        }
+
+        // GET: api/<OrdersController>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<OrderDTO>>> GetOrderDTOsAsync()
+        {
+            var orders = await _orderRepository.GetOrderDTOsAsync();
+
+            if (orders == null || !orders.Any())
+            {
+                return NotFound();
+            }
+
+            return Ok(orders);
+        }
+
+        [HttpGet("newId")]
+        public async Task<ActionResult<string>> GetNewOrderIdAsync()
+        {
+            string newId = await _orderRepository.GetNewOrderIdAsync();
+
+            return Ok(newId);
+        }
+
+        // GET api/<OrdersController>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<OrderDTO>> GetOrderDTOByIdAsync(string id)
+        {
+            var order = await _orderRepository.GetOrderDTOByIdAsync(id);
+
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(order);
+        }
+
+        // POST api/<OrdersController>
+        [HttpPost]
+        public async Task<ActionResult> CreateOrderAsync([FromBody] OrderDTO orderDTO)
+        {
+            try
+            {
+                await _orderRepository.AddOrderDTOAsync(orderDTO);
+
+                return Ok("Order created successfully.");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogInformation($"Error message: {ex.Message}");
+                return StatusCode(500, $"An error occurred while creating the order. OrderId: {orderDTO.OrderId}");
+            }
+        }
+
+        // PUT api/<OrdersController>/5
+        [HttpPut("{id}")]
+        public async Task<ActionResult> UpdateOrderAsync(string id, [FromBody] OrderDTO orderDTO)
+        {
+            try
+            {
+                await _orderRepository.UpdateOrderAsync(orderDTO);
+
+                return Ok("Order updated successfully.");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogInformation($"Error message: {ex.Message}");
+                return StatusCode(500, $"An error occurred while creating the order. OrderId: {orderDTO.OrderId}");
+            }
+        }
+
+        // DELETE api/<OrdersController>/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteOrderAsync(string id)
+        {
+            try
+            {
+                await _orderRepository.DeleteOrderAsync(id);
+
+                return Ok("Order updated successfully.");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogInformation($"Error message: {ex.Message}");
+                return StatusCode(500, $"An error occurred while creating the order. OrderId: {id}");
+            }
+        }
+    }
+}
