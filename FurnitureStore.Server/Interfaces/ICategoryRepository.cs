81ca63cd7f61d5fcb0f58e434839cb28c8f8cca5
TrungKien <trungkien2003ntk@gmail.com>
2023-12-19 18:21:29 +0700

    Implement repository pattern

---
+++ FurnitureStore.Server/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,17 @@
+﻿using FurnitureStore.Server.Models.Documents;
+using FurnitureStore.Shared;
+
+namespace FurnitureStore.Server.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        Task AddCategoryDocumentAsync(CategoryDocument item);
+        Task UpdateCategoryAsync(CategoryDTO item);
+        Task AddCategoryDTOAsync(CategoryDTO item);
+
+        Task<IEnumerable<CategoryDTO>> GetCategoryDTOsAsync();
+        Task<CategoryDTO> GetCategoryDTOByIdAsync(string id);
+        Task DeleteCategoryAsync(string id);
+        Task<string> GetNewCategoryIdAsync();
+    }
+}
