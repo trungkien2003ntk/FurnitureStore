52082a2a377e42e0d9c7e82b8243f18cf68a8314
Do Nhat <dmnhat.pt@gmail.com>
2024-01-11 10:24:33 +0700

    Products service.

---
+++ FurnitureStore.Client/IServices/IProductService.cs
@@ -0,0 +1,12 @@
+﻿using FurnitureStore.Shared.DTOs;
+
+namespace FurnitureStore.Client.IServices
+{
+    public interface IProductService
+    {
+        Task<ProductDTO> AddProductAsync(ProductDTO productDTO);
+        Task<bool> UpdateProductAsync(string productId, ProductDTO productDTO);
+        Task<bool> DeleteProductAsync(string productId);
+        Task<IEnumerable<ProductDTO>> GetProductDTOsByPageSizeAndPageNumber(int pageSize, int pageNumber);
+    }
+}
