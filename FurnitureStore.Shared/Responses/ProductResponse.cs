b8f4045496991597077fff2b2c359fbbeac0c6a0
TrungKien <trungkien2003ntk@gmail.com>
2024-01-11 14:39:45 +0700

    Create a response for product to deserialise json

---
+++ FurnitureStore.Shared/Responses/ProductResponse.cs
@@ -0,0 +1,12 @@
+﻿using FurnitureStore.Shared.DTOs;
+
+namespace FurnitureStore.Shared.Responses;
+
+public class ProductResponse
+{
+    [JsonProperty("data")]
+    public List<ProductDTO> Data { get; set; }
+
+    [JsonProperty("metadata")]
+    public Metadata Metadata { get; set; }
+}
