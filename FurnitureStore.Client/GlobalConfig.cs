ae70959c46ef0a7b88472c8a98a4ca6cf6ec37ca
Nhatduy13nhatduy <nhatduy13nhatduy@gmail.com>
2024-01-11 23:47:13 +0700

    Update Nhat

--- FurnitureStore.Client/GlobalConfig.cs
+++ FurnitureStore.Client/GlobalConfig.cs
@@ -2,7 +2,7 @@
 {
     public class GlobalConfig
     {
-        public const string BASE_ENDPOINT = "localhost:7007";
+        public const string BASE_ENDPOINT = "furniturestore-uit-y3s3.azurewebsites.net";
         public const string BASE_URL = $"https://{BASE_ENDPOINT}/api/";
         public const string PRODUCT_BASE_URL = $"{BASE_URL}Products";
         public const string CATEGORY_BASE_URL = $"{BASE_URL}Categories";
