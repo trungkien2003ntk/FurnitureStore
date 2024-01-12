282dcd74eab6f50abd093aa907cd722c45f61492
Thuy Trinh ne <21522719@gm.uit.edu.vn>
2024-01-12 08:28:21 +0700

    Rever Changes

--- FurnitureStore.Client/IServices/ICategoryService.cs
+++ FurnitureStore.Client/IServices/ICategoryService.cs
@@ -6,12 +6,11 @@ namespace FurnitureStore.Client.IServices
     public interface ICategoryService
     {
         Task<IEnumerable<CategoryResponse>> GetAllCategoryResponsesWithNestedResult();
-        Task<CategoryDTO> GetCategoryDTOsById(string id);
         Task<CategoryDTO> AddCategoryAsync(CategoryDTO categoryDTO);
         Task<bool> DeleteCategoryDTOAsync(string categoryId);
         Task<bool> UpdateCategoryDTOAsync(string categoryId, CategoryDTO categoryDTO);
         Task<CategoryDTO> GetCategoryDTOByIdAsync(string categoryId);
-        Task<IEnumerable<CategoryResponse>?> GetCategoryDTOsByLevelAsync(int level);
-        Task<IEnumerable<CategoryResponse>?> GetCategoryDTOsByParentIdAsync(string parentId);
+        Task<IEnumerable<CategoryDTO>> GetCategoryDTOsByLevelAsync(int level);
+        Task<IEnumerable<CategoryDTO>> GetCategoryDTOsByParentIdAsync(string parentId);
     }
 }
